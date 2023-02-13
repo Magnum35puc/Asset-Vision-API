358b011d63c2b8d526904d53c6c8c0008d594454
Théo Px <magnum35puc@gmail.com>
2023-02-14 00:04:21 +0100

    setup_mongo

--- setup_mongo.py
+++ setup_mongo.py
@@ -8,14 +8,4 @@ db = client.AssetVision
 assets = db.assets
 portfolios = db.portfolios
 
-#db.assets.create_index([("symbol", ASCENDING)],unique=True)
-
-
-for portfolio in portfolios.find():
-    print(portfolio)
-    asset_symbols = assets.find({"portfolio_id": portfolio["_id"]}).distinct("symbol")
-    portfolio_assets = []
-    for symbol in asset_symbols:
-        asset = assets.find_one({"symbol": symbol})
-        portfolio_assets.append(asset)
-    print(portfolio_assets)
+db.assets.create_index([("symbol", ASCENDING)],unique=True)
